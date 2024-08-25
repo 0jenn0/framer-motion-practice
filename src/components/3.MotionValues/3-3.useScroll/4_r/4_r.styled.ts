f7e87a1a603bb2b1bdfb01762fc7ca850f9550e3
river <130737187+0jenn0@users.noreply.github.com>
2024-08-25 21:46:16 +0900

    feat(4_r): useScroll 4번째 예제 ) : 컨테이너 scrollYprogress에 따라 scaleX가 달라지는 div 구현

--- src/components/3.MotionValues/3-3.useScroll/4_r/4_r.styled.ts
+++ src/components/3.MotionValues/3-3.useScroll/4_r/4_r.styled.ts
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
 export const Layout = styled.div`
+  position: relative;
   width: 40rem;
   height: 40rem;
-  overflow-y: scroll;
 
   background-color: #e62c2c;
   border-radius: 10px;
@@ -11,5 +12,13 @@ export const Layout = styled.div`
   display: flex;
   flex-direction: column;
 
-  gap: 2rem;
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+`;
+
+export const MotionDiv = styled(motion.div)`
+  height: 0.2rem;
+  background-color: #fff;
+  width: 40rem;
 `;
--- src/components/3.MotionValues/3-3.useScroll/4_r/4_r.tsx
+++ src/components/3.MotionValues/3-3.useScroll/4_r/4_r.tsx
@@ -1,13 +1,24 @@
+import { useScroll, useSpring } from "framer-motion";
 import * as S from "./4_r.styled";
 import Item from "./component/Item";
+import { useRef } from "react";
 
 const UseScrollSelf04 = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    container: ref,
+  });
+  const scaleX = useSpring(scrollYProgress);
+
   return (
-    <S.Layout>
-      {Array.from({ length: 5 }, (_, index) => {
-        return <Item key={index} index={index} />;
-      })}
-    </S.Layout>
+    <>
+      <S.Layout ref={ref}>
+        {Array.from({ length: 5 }, (_, index) => {
+          return <Item key={index + 1} id={index} containRef={ref} />;
+        })}
+      </S.Layout>
+      <S.MotionDiv style={{ scaleX }} />
+    </>
   );
 };
 
