143b29097e4965b48ae0e48ee2e02b6e7a4d9986
river <130737187+0jenn0@users.noreply.github.com>
2024-08-25 17:20:25 +0900

    feat: useScroll 4번째 예제 이미지 asset 추가

---
+++ src/components/3.MotionValues/3-3.useScroll/4_r/images/index.ts
@@ -0,0 +1,5 @@
+export { default as Image01 } from "./1.jpg";
+export { default as Image02 } from "./2.jpg";
+export { default as Image03 } from "./3.jpg";
+export { default as Image04 } from "./4.jpg";
+export { default as Image05 } from "./5.jpg";
