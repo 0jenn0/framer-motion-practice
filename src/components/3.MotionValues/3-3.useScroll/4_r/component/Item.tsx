845979633a8ee8db0c5c6944b9019d36a8db84a7
river <130737187+0jenn0@users.noreply.github.com>
2024-08-25 21:45:15 +0900

    feat(Item) : useScroll 4번째 예제 ) Item의 text가 scrollYProgress에 따라 애니메이션 되도록 구현
    - 외부에서 스크롤 컨테이너 ref를 props로 받도록 구현

--- src/components/3.MotionValues/3-3.useScroll/4_r/component/Item.tsx
+++ src/components/3.MotionValues/3-3.useScroll/4_r/component/Item.tsx
@@ -1,18 +1,32 @@
+import React, { Ref, useRef } from "react";
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import * as S from "./Item.styled";
 import { Image01, Image02, Image03, Image04, Image05 } from "../images";
 
 const images = [Image01, Image02, Image03, Image04, Image05];
 
 interface ItemProps {
-  index: number;
+  id: number;
+  containRef: React.RefObject<HTMLElement>;
 }
 
-const Item = ({ index }: ItemProps) => {
+const Item = ({ id: index, containRef }: ItemProps) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    container: containRef,
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
   const image = images[index];
+
   return (
     <S.Layout>
-      <S.Image src={image.src} alt={`Image ${index + 1}`} />
-      <S.Text>#00{index + 1}</S.Text>
+      <div ref={ref}>
+        <S.Image src={image.src} alt={`Image ${index + 1}`} />
+      </div>
+      <S.MotionText layout style={{ y }}>{`#00${index + 1}`}</S.MotionText>
     </S.Layout>
   );
 };
